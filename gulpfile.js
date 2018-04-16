b80f9c5081dd37536009897b8bf4297aecb5dc45
null <nullhund@protonmail.com>
2018-04-15 21:31:32 -0400

    include keybase proof in upload

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let gulp = require('gulp');
 var s3 = require('gulp-s3-upload')();
 
 gulp.task('deploy', function() {
-    gulp.src('./index.html')
+    gulp.src(['./index.html', './keybase.txt'])
         .pipe(s3({
             Bucket: cfg.s3_bucket,
             ACL:    'public-read'
